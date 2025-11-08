ec7273db99ee9b09fb5c5ab2f354ec85f6175433
tremckinley <tremckinley901@gmail.com>
2025-11-08 12:36:21 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import RFPDashboard from './RFPDashboard'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <RFPDashboard />
+    </>
+  )
+}
+
+export default App
